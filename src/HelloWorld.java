722f3312a8783750ed6c15d71739e0cf5a318891
Bennie-Lee <80985718+Bennie-Lee@users.noreply.github.com>
2021-04-22 21:22:26 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Name: Bennie Lee");
+        System.out.println("Hometown: Bear, DE");
+        System.out.println("Major: Computer Science");
+        System.out.println("Favorite Song: Backyard by Travis Scott ");
+        System.out.println("Favorite Superhero(s): Spiderman");
+        System.out.println("Favorite Caffeine Source: Not a big fan of caffeine :( ");
+    }
+}
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+            System.out.println("Name: Bennie Lee");
+            System.out.println("Hometown: Bear, DE");
+            System.out.println("Major: Computer Science");
+            System.out.println("Favorite Song: Backyard by Travis Scott ");
+            System.out.println("Favorite Superhero(s): Spiderman");
+            System.out.println("Favorite Caffeine Source: Not a big fan of caffeine :( ");
+
+        }
+    }
